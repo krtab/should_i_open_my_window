6c8bd677b2fbdf7463ba90111ddae1e53a9ff121
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2024-10-25 23:45:09 +0200

    a few improvements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use chrono::{DurationRound, TimeDelta};
 use clap::Parser;
-use comfy_table::{Cell, Cells, Table};
+use comfy_table::{
+    presets::{ASCII_FULL_CONDENSED, UTF8_FULL_CONDENSED},
+    Cell, Cells, Table,
+};
 
 const TEMP_RANGE: [f64; 13] = [
     16., 16.5, 17., 17.5, 18., 18.5, 19., 19.5, 20., 20.5, 21., 21.5, 22.,
@@ -14,6 +17,9 @@ static DOC_STR: &str = "Opening the window will bring indoor humidity closer \
 struct Args {
     lat: f64,
     lng: f64,
+    /// Forces output to use ASCII only
+    #[clap(long)]
+    ascii: bool,
 }
 
 #[tokio::main]
@@ -22,13 +28,11 @@ async fn main() {
     let mut opts = open_meteo_rs::forecast::Options::default();
 
     // Location
-    let Args { lat, lng } = Args::parse();
+    let args @ Args { lat, lng, .. } = Args::parse();
     opts.location = open_meteo_rs::Location { lat, lng };
     opts.elevation = Some(63.1.into());
     opts.temperature_unit = Some(open_meteo_rs::forecast::TemperatureUnit::Celsius);
-    opts.wind_speed_unit = Some(open_meteo_rs::forecast::WindSpeedUnit::Kmh);
-    opts.precipitation_unit = Some(open_meteo_rs::forecast::PrecipitationUnit::Millimeters);
-    opts.time_zone = Some(chrono_tz::Europe::Paris.name().into());
+    opts.time_zone = Some("auto".to_owned());
 
     // Past days (0-2)
     // opts.past_days = Some(2); // !! mutually exclusive with dates
@@ -48,7 +52,7 @@ async fn main() {
         let cell = Cell::new(format!("{temp:.1}°C")).add_attribute(comfy_table::Attribute::Bold);
         header.push(cell);
     }
-    table.add_row(Cells(header));
+    table.set_header(Cells(header));
     let now = chrono::offset::Local::now()
         .naive_local()
         .duration_trunc(TimeDelta::hours(1))
@@ -73,6 +77,11 @@ async fn main() {
             row.push(rh_cell(rh));
         }
         table.add_row(Cells(row));
+    }
+    if args.ascii {
+        table.load_preset(ASCII_FULL_CONDENSED);
+    } else {
+        table.load_preset(UTF8_FULL_CONDENSED);
     }
     println!("{}\n", DOC_STR);
     println!("{table}");
